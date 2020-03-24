06e80266862c2547b2ebad0492c1b76d33b46ce4
Frederik Rabøl <frederik-rm@hotmail.com>
2020-03-24 22:07:35 +0100

    e93ea12

---
+++ precache-manifest.3aae3a9d0ac7d6e3adc0f593664c096e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d16c090b11e14fabe447",
+    "url": "/js/chunk-vendors.c05aa06d.js"
+  },
+  {
+    "revision": "93638eb9b2ddd24bd55c",
+    "url": "/js/app.9fd55fb1.js"
+  },
+  {
+    "revision": "94a75288fdf65d2e891c",
+    "url": "/js/about.87cf35ae.js"
+  },
+  {
+    "revision": "3cf6096ccd41a88a06c67f0b51e3b644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598907574484ac04def471e338e4aa03",
+    "url": "/img/preview.png"
+  },
+  {
+    "revision": "93638eb9b2ddd24bd55c",
+    "url": "/css/app.74935a0c.css"
+  },
+  {
+    "revision": "9d430a16ad80acc906b30ed36bf9452f",
+    "url": "/CNAME"
+  }
+];
